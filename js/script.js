79def5cdaebfb208c7f83a1d1c25d6f0eb548e07
Robatortas <robatortas1@gmail.com>
2022-03-09 21:32:53 -0800

    transition effects

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 // I HATE JAVASCRIPT!
 // FUCK IT!
 
+document.addEventListener("scroll", () => {
+    document.documentElement.dataset.scroll = window.scrollY;
+});
+
+$(".section").scroll(function() {
+    if ($(this).scrollTop() > 50) {
+      $(".generalHeading").css("display", "none").fadeOut("slow");
+    } else {
+      // do something else
+    }
+  });
