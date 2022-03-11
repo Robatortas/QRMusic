0b853e1ca8104a81fdb8c63ef38b19c775d109c1
Robatortas <robatortas1@gmail.com>
2022-03-10 18:04:36 -0800

    Working on JS scroll EventListener

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
 // I HATE JAVASCRIPT!
 // FUCK IT!
 
-document.addEventListener("scroll", () => {
-    document.documentElement.dataset.scroll = window.scrollY;
+// $(window).scroll(function() {
+//   var a = 1;
+//   var pos = $(window).scrollTop();
+//   if(pos > a) {
+//         $("search").css({
+//               position: 'fixed'
+//         });
+//   } else {
+//     $("search").css({
+//             position: 'absolute',
+//             top:'600px'
+//     });
+//   }
+// });
+
+document.addEventListener("scroll", function() {
+  let scrollDemo = document.querySelector("body");
+  // document.body.innerHTML = scrollDemo.scrollTop;
+  console.log(scrollDemo.scrollTop);
 });
 
-$(".section").scroll(function() {
-    if ($(this).scrollTop() > 50) {
-      $(".generalHeading").css("display", "none").fadeOut("slow");
-    } else {
-      // do something else
-    }
-  });
+function scrollDetect() {
+  var a = 1;
+  var pos = $(window).scrollTop();
+  if(pos > a) {
+        $("search").css({
+              position: 'fixed'
+        });
+  } else {
+    $("search").css({
+            position: 'absolute',
+            top:'600px'
+    });
+  }
+}
